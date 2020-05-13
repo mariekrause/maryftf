04a9d12748998ad8a91df83b1af7ff80fdac351a
Marie Krause <marie.krause48@web.de>
2020-05-13 15:04:02 +0200

    hot fix on about page responsiveness

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,25 +1,36 @@
 <template>
     <div class="aboutme">
         <site-header></site-header>
-        <div class="content">
-          <img id="keyvisual" src="../assets/avatar_skills.png">
-          <div class="content-right">
-          <h1 id="heading">Willkommen in meiner Welt.</h1>
-            <p class="subline1">Die Kurzfassung:</p>
-            <span id="first_impression">Mein Name ist <strong>Marie Krause</strong>, ich bin 20 Jahre alt und studiere im 4. Semester 
-            <strong>Digitale Medien an der DHBW Mannheim</strong>. Und zwar Dual - das heißt, ich kenne mich nicht nur mit der Theorie aus, sondern sammle parallel 
-              auch Erfahrungen im praktischen Unternehmensumfeld.
-            </span>
-            <p class="line">Mein Studium gibt mir eine Bandbreite an Wissensgebieten mit auf den Weg - <strong>Wirtschaft, Informatik und Gestaltung</strong>.
-              Doch nichts kann meinen Willen ersetzen, ständig Neues zu lernen. Offenheit, grenzenlose Neugier und meine Begeisterung für
-              neues Wissen machen mich aus.
-            </p>
-            <p class="line">In meiner Freizeit bin ich für mein Leben gern unterwegs - Deutschland, Europa, weltweit - am liebsten mit dem Zug. Alles über meine
-              Abenteuer erfahrt Ihr <a href="https://www.instagram.com/maryftf" target=_blank>auf Instagram</a> oder <a href="/blog">auf meinem Blog</a>.
-            </p>
-            <p class="subline1">Willst du mehr erfahren? Dann kontaktiere mich doch <a href="/contact">hier</a>.</p>
-          </div>
-        </div>
+        <b-container fluid id="wrapper">
+        <b-col> 
+          <b-row>
+            <b-col xs="11" md="11" lg="7" xl="7">
+              <div class="content-right">
+                <h1 id="heading">Willkommen in meiner Welt.</h1>
+                <p class="subline1">Die Kurzfassung:</p>
+                <span id="first_impression">Mein Name ist <strong>Marie Krause</strong>, ich bin 20 Jahre alt und studiere im 4. Semester 
+                <strong>Digitale Medien an der DHBW Mannheim</strong>. Und zwar Dual - das heißt, ich kenne mich nicht nur mit der Theorie aus, sondern sammle parallel 
+                auch Erfahrungen im praktischen Unternehmensumfeld.
+                </span>
+                <p class="line">Mein Studium gibt mir eine Bandbreite an Wissensgebieten mit auf den Weg - <strong>Wirtschaft, Informatik und Gestaltung</strong>.
+                Doch nichts kann meinen Willen ersetzen, ständig Neues zu lernen. Offenheit, grenzenlose Neugier und meine Begeisterung für
+                neues Wissen machen mich aus.
+                </p>
+                <p class="line">In meiner Freizeit bin ich für mein Leben gern unterwegs - Deutschland, Europa, weltweit - am liebsten mit dem Zug. Alles über meine
+                Abenteuer erfahrt Ihr <a href="https://www.instagram.com/maryftf" target=_blank>auf Instagram</a> oder <a href="/blog">auf meinem Blog</a>.
+                </p>
+                <p class="subline1">Willst du mehr erfahren? Dann kontaktiere mich doch <a href="/contact">hier</a>.</p>
+              </div>
+            </b-col>
+            <b-col class="d-none d-lg-inline">
+              <img id="keyvisual" src="../assets/avatar_skills.png" lg="4" xl="4">
+            </b-col>
+          </b-row>
+          <b-row class="smallonly d-lg-none d-md-inline d-xs-inline">
+            <img id="keyvisual2" src="../assets/avatar_skills.png">
+          </b-row>
+        </b-col>
+        </b-container>
         <custom-footer id="bottom"></custom-footer>
     </div>
 </template>
@@ -56,20 +67,10 @@ export default {
   .line {
     margin-top: 0
   }
-  .aboutme {
-    position: relative;
-    height: 100vh;
-  }
   #bottom {
     position: fixed;
     bottom: 0;
     width: 100%
-  }
-  .content {
-    display: flex;
-    flex-direction: row;
-    position: relative;
-    height: 80vh
   }
   #first_impression {
     padding-bottom: 2vh;
@@ -82,8 +83,6 @@ export default {
   .content-right {
     display: flex;
     flex-direction: column;
-    margin-right: 50vw;
-    margin-left: 5vw;
     line-height: 1.5;
   }
   #heading {
@@ -93,11 +92,22 @@ export default {
     margin-bottom: 0
   }
   #keyvisual {
-    max-height: 70vh;
-    padding-left: 5vw;
-    padding-top: 10vh;
+    width: 100%;
+    height: auto;
     position: absolute;
     right: 1vw;
     bottom: 0
+  }
+  #keyvisual2 {
+    width: 90vw;
+    height: auto;
+    position: inherit;
+    margin-bottom: 10%
+  }
+  #wrapper {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    max-width: 1200px;
   }
 </style>
